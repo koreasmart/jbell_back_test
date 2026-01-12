f12356ed42d6fd75d1678bc7c3edd37984e49900
koreasmart <coolhongs@naver.com>
2026-01-12 12:28:33 +0900

    [team-팀원] 프로젝트 생성 detail : 프로젝트 초기설정

---
+++ jbell_backend/src/main/java/jbell/common/interceptor/LogInterceptor.java
@@ -0,0 +1,71 @@
+package jbell.common.interceptor;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Set;
+import java.util.StringJoiner;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor{
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		
+		Set<String> paramKey = request.getParameterMap().keySet();
+		
+		var param = new StringJoiner(", ");
+		
+		
+		for(String key : paramKey) {
+			param.add(key + ": " + request.getParameter(key));
+		}
+		
+		String requestBody = null;
+		if(request instanceof ContentCachingRequestWrapper) {
+			
+			ContentCachingRequestWrapper wrapperRequest = (ContentCachingRequestWrapper) request;
+			
+			byte[] content = wrapperRequest.getContentAsByteArray();
+			if(content.length > 0) {
+				try {
+					requestBody = new String(content, StandardCharsets.UTF_8);
+				} catch (Exception e) {
+					log.error("Failed to log request body", e);
+				}
+			}
+		}
+		
+		log.info("=====================Access Log Start==========================");
+		log.info("PORT		::::	{}", request.getLocalPort());
+		log.info("SERVERNAME	::::	{}", request.getServerName());
+		log.info("HTTP METHOD	::::	{}", request.getMethod());
+		log.info("URI		::::	{}", request.getRequestURI());
+		log.info("CLIENT IP	::::	{}", request.getRemoteAddr());
+		if(param.length() > 0) {			
+			log.info("PARAMETER	::::	{}", param);
+		}
+		if(requestBody != null) {			
+			log.info("REQUEST BODY	::::	{}", requestBody.replaceAll("\\s+", ""));
+		}
+		log.info("====================Access Log End==============================");
+		
+		
+		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+	}
+}
+
+
+
+
+
+
+
---
+++ jbell_backend/src/main/java/jbell/config/SecurityConfig.java
@@ -0,0 +1,75 @@
+package jbell.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+	
+	private static String[] RERMIT_REQEUST_URI = {
+			 "/api/auth/**"
+			,"/oauth2/login"
+	};
+	
+	private final CorsConfigurationSource corsConfigurationSource;
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			// csrf, formlogin, httpBasic 비활성화
+			.csrf(csrf -> csrf.disable())
+			.formLogin(form -> form.disable())
+			.httpBasic(basic -> basic.disable())
+			// 세션비활성화
+			.sessionManagement(session -> session
+											.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			)
+			// CORS 설정
+			.cors(cors -> cors.configurationSource(corsConfigurationSource))
+			// 주소
+			.authorizeHttpRequests(auth -> auth
+											 .requestMatchers(RERMIT_REQEUST_URI).permitAll()
+											 .anyRequest().authenticated()
+			);
+		return http.build();
+	}
+	
+	/**
+	 * 정적 리소스 제외 설정
+	 * 설정된 주소는 Security Filter를 거치지 않는다.
+	 */
+	@Bean
+	WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring()
+						   .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+						   .requestMatchers("/favicon.*", "/resources/**", "/error");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
